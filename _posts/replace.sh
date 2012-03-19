b063b18e1d8cc4d70b580d91dd3156a0ed6ffc9e
davide <lastexxit@gmail.com>
2012-03-19 13:03:36 +0100

    syntax for php

--- _posts/replace.sh
+++ _posts/replace.sh
@@ -3,8 +3,8 @@ for file in *.html
 do
 	perl -p -i -e 's/\[ruby\]/\{\% highlight ruby \%\}/g' "$file"
 	perl -p -i -e 's/\[\/ruby\]/\{\% endhighlight \%\}/g' "$file"
-	#perl -p -i -e 's/\[php\]/\{\% highlight php startinline \%\}/g' "$file"
-	#perl -p -i -e 's/\[\/php\]/\{\% endhighlight \%\}/g' "$file"
+	perl -p -i -e 's/\[php\]/\{\% highlight php startinline \%\}/g' "$file"
+	perl -p -i -e 's/\[\/php\]/\{\% endhighlight \%\}/g' "$file"
 	#perl -p -i -e 's/\[bash\]/\{\% highlight bash \%\}/g' "$file"
 	#perl -p -i -e 's/\[\/bash\]/\{\% endhighlight \%\}/g' "$file"
 	#perl -p -i -e 's/\&lt;/::/g' "$file"
