e54a0f5c9e46dcc3d371e73fdb694dc87dfa2909
davide <lastexxit@gmail.com>
2012-03-19 13:33:11 +0100

    probably fixing the problem with a bash syntax file

--- _posts/replace.sh
+++ _posts/replace.sh
@@ -5,8 +5,8 @@ do
 	perl -p -i -e 's/\[\/ruby\]/\{\% endhighlight \%\}/g' "$file"
 	perl -p -i -e 's/\[php\]/\{\% highlight php startinline \%\}/g' "$file"
 	perl -p -i -e 's/\[\/php\]/\{\% endhighlight \%\}/g' "$file"
-	perl -p -i -e 's/\[bash\]/\{\% highlight bash \%\}/g' "2011-07-18-netbeans-e-phpunit.html"
-	perl -p -i -e 's/\[\/bash\]/\{\% endhighlight \%\}/g' "2011-07-18-netbeans-e-phpunit.html"
+	perl -p -i -e 's/\[bash\]/\{\% highlight bash \%\}/g' "$file"
+	perl -p -i -e 's/\[\/bash\]/\{\% endhighlight \%\}/g' "$file"
 	perl -p -i -e 's/\&lt;/::/g' "$file"
-	#perl -p -i -e 's/\r\n$/\n/g' "$file"
+	perl -p -i -e 's/\r\n$/\n/g' "$file"
 done
