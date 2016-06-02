e089930baf0b92a23a90de84277e4955c04d963d
edapx <lastexxit@gmail.com>
2016-06-02 22:01:16 +0200

    clean and make it work

--- terrain/index.js
+++ terrain/index.js
@@ -1,6 +1,8 @@
-var container, camera, controls, scene, renderer, stats;
-var displacement, noise, uniforms;
-var camera_z_position = 1000;
+var container, camera, controls, scene, renderer, stats, gui;
+var bumpScale;
+var camera_z_position = 500;
+
+var terrain;
 
 //if model loaded OK
 init();
@@ -12,6 +14,13 @@ function init() {
     controls = new THREE.OrbitControls( camera );
     controls.addEventListener( 'change', render );
     scene = new THREE.Scene();
+    bumpScale = 200.0;
+
+
+    // Create Light
+    var light = new THREE.PointLight(0xFFFFFF);
+    light.position.set(0, 0, 500);
+    scene.add(light);
 
     renderer = new THREE.WebGLRenderer( { antialias: true, depth:true} );
     renderer.setSize( window.innerWidth, window.innerHeight );
@@ -20,18 +29,58 @@ function init() {
     container = document.getElementById( 'container' );
     container.appendChild( renderer.domElement );
 
+    window.addEventListener( 'resize', onWindowResize, false );
+
+    var bumpTexture = loadTexture( 'noise.png' );
+    var customMaterial = createCustomMaterial( bumpTexture );
+
+    var geometryPlane = new THREE.PlaneBufferGeometry(2000, 2000, 50, 50);
+    geometryPlane.rotateX( - Math.PI / 2.5);
+    terrain = new THREE.Mesh( geometryPlane, customMaterial );
+    scene.add( terrain );
+
+    addGui( customMaterial );
+    addStats();
+}
+
+function createCustomMaterial( texture ) {
+    //var material = new THREE.MeshBasicMaterial({ map : groundTexture})
+    var myUniforms = {
+        bumpScale: {type: 'f', value: bumpScale},
+        bumpTexture: {type: 't', value: texture}
+    };
+
+    var customMaterial = new THREE.ShaderMaterial({
+        uniforms: myUniforms,
+        vertexShader: document.getElementById('vertexShader').textContent,
+        fragmentShader: document.getElementById('fragmentShader').textContent,
+    });
+
+    return customMaterial;
+}
+
+function loadTexture( filename ){
+    var loadingManager = new THREE.LoadingManager( function(){
+        terrain.visible = true;
+    });
+    var textureLoader = new THREE.TextureLoader( loadingManager );
+    var bumpTexture = textureLoader.load('noise.png');
+    bumpTexture.wrapS = bumpTexture.wrapT = THREE.RepeatWrapping;
+
+    return bumpTexture;
+}
+
+function addGui(customMaterial){
+    gui = new dat.GUI();
+    gui.add(customMaterial.uniforms.bumpScale, 'value')
+        .name('bumpScale').min(20).max(300).step(1.0);
+    gui.close();
+}
+
+function addStats(){
     stats = new Stats();
     stats.showPanel( 0 );
     container.appendChild(stats.domElement);
-    window.addEventListener( 'resize', onWindowResize, false );
-    //Texture
-// stavi iniziando le texture da qui https://github.com/mrdoob/three.js/blob/master/examples/webgl_terrain_dynamic.html
-                    var loadingManager = new THREE.LoadingManager( function(){
-                    terrain.visible = true;
-                });
-                var textureLoader = new THREE.TextureLoader( loadingManager );
-
-    createTerrain();
 }
 
 function onWindowResize() {
@@ -50,12 +99,4 @@ function animate() {
 
 function render() {
     renderer.render( scene, camera );
-}
-
-function createTerrain(){
-    var geometry = new THREE.PlaneBufferGeometry(2000, 2000, 20);
-    geometry.rotateX( - Math.PI / 2.5);
-    var material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
-    var plane = new THREE.Mesh( geometry, material );
-    scene.add( plane );
 }
