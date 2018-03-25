8e7194311364e6155449bc0e90fdd342d8a07ce4
Davide Prati <lastexxit@gmail.com>
2018-03-25 13:02:26 +0200

    multiple materials

--- shaders/ray-marching-tutorial/25.glsl
+++ shaders/ray-marching-tutorial/25.glsl
@@ -52,8 +52,21 @@ float bendTorusB( vec3 p, vec2 dim ){
     return sdTorus(q, dim);
 }
 
+vec2 opU( vec2 d1, vec2 d2 ){
+    return (d1.x < d2.x) ? d1 : d2;
+}
 
-float map(vec3 pos){
+vec2 sminsMat(vec2 d1, vec2 d2){
+    float d = smin(d1.x, d2.x, 3.0);
+    vec2 d1Mat = vec2(d, d1.y);
+    vec2 d2Mat = vec2(d, d2.y);
+    return (d1.x < d2.x) ? d1Mat : d2Mat;
+}
+
+// multiple materials. The x component returns the distance
+// field value. The y the material
+vec2 map(vec3 pos){
+    vec2 res = vec2(0.,0.);
     float diam = 3.;
     float thick = 0.6;
     
@@ -68,7 +81,12 @@ float map(vec3 pos){
     float s2 = bendTorusB(s2pos, vec2(diam, thick));
     float s3 = bendTorusB(s3pos, vec2(diam, thick));
 
-    return smins(s1, smins(s2, s3));
+    vec2 s1Mat = vec2(s1, 0.1); // 0.1 is an arbitrary value for the material
+    vec2 s2Mat = vec2(s2, 0.2);
+    vec2 s3Mat = vec2(s3, 0.3);
+    //return smins(s1, smins(s2, s3));
+    res = sminsMat(s1Mat,sminsMat(s2Mat, s3Mat));
+    return res;
 }
 
 vec2 squareFrame(vec2 res, vec2 coord){
@@ -77,28 +95,31 @@ vec2 squareFrame(vec2 res, vec2 coord){
     return uv;
 }
 
-float raymarching(vec3 eye, vec3 marchingDirection){
-    float depth = NEAR_CLIP;
+vec2 raymarching(vec3 eye, vec3 marchingDirection){
+    // the x value store the depth distance, the y the material
+    vec2 res = vec2(NEAR_CLIP, 0.);
     for (int i = 0; i < MAX_MARCHING_STEPS; i++) {
-        float dist = map(eye + depth * marchingDirection);
-        if (dist < EPSILON){
-            return depth;
+        vec2 distAndMat = map(eye + res.x * marchingDirection);
+        res.y = distAndMat.y;
+        if (distAndMat.x < EPSILON){
+            return res;
         }
+        res.x += distAndMat.x;
 
-        depth += dist;
-
-        if (depth >= FAR_CLIP) {
-            return FAR_CLIP;
+        if (distAndMat.x >= FAR_CLIP) {
+            res.x = FAR_CLIP;
+            return res;
         }
     }
-    return FAR_CLIP;
+    res.x = FAR_CLIP;
+    return res;
 }
 
 float softshadow( in vec3 ro, in vec3 rd, in float mint, in float tmax ) {
 	float res = 1.0;
     float t = mint;
     for( int i=0; i<16; i++ ) {
-		float h = map( ro + rd*t );
+		float h = map( ro + rd*t ).x;
         res = min( res, 8.0*h/t );
         t += clamp( h, 0.02, 0.10 );
         if( h<0.001 || t>tmax ) break;
@@ -114,7 +135,7 @@ float ao( in vec3 pos, in vec3 nor ){
     {
         float hr = 0.01 + 0.06*float(i)/4.0;
         vec3 aopos =  nor * hr + pos;
-        float dd = map( aopos );
+        float dd = map( aopos ).x;
         occ += -(dd-hr)*sca;
         sca *= 0.95;
     }
@@ -124,9 +145,9 @@ float ao( in vec3 pos, in vec3 nor ){
 vec3 computeNormal(vec3 pos){
     vec2 eps = vec2(0.01, 0.);
     return normalize(vec3(
-        map(pos + eps.xyy) - map(pos - eps.xyy),
-        map(pos + eps.yxy) - map(pos - eps.yxy),
-        map(pos + eps.yyx) - map(pos - eps.yyx)
+        map(pos + eps.xyy).x - map(pos - eps.xyy).x,
+        map(pos + eps.yxy).x - map(pos - eps.yxy).x,
+        map(pos + eps.yyx).x - map(pos - eps.yyx).x
     ));
 }
 
@@ -145,19 +166,29 @@ float fresnel(vec3 normal, vec3 dir){
     return pow( clamp(1.0+dot(normal,dir),0.0,1.0), 2.0 );
 }
 
-vec3 getRefTexture(vec3 normal, vec3 dir) {
+vec3 getRefTexture(vec3 normal, vec3 dir, float mat) {
     vec3 eye = -dir;
 	vec3 r = reflect( eye, normal );
     //eastern-rosella-big.jpg
-    vec4 color = texture2D(iChannel0, (0.5 * (r.xy) + .5));
+    vec4 color;
+    if(mat == 0.1){
+        color = texture2D(iChannel1, (0.5 * (r.xy) + .5));
+    }
+    else if(mat == 0.2){
+        color = texture2D(iChannel2, (0.5 * (r.xy) + .5));
+    }
+    else if(mat == 0.3){
+        color = texture2D(iChannel3, (0.5 * (r.xy) + .5));
+    }
+
     return color.xyz;
 }
 
-vec3 calculateColor(vec3 pos, vec3 dir){
+vec3 calculateColor(vec3 pos, vec3 dir, float mat){
   vec3 normal = computeNormal(pos);
   vec3 color;
 
-  vec3 colTex = getRefTexture(normal, dir);
+  vec3 colTex = getRefTexture(normal, dir, mat);
   float diffLight = diffuse(normal);
   float specLight = specular(normal, dir);
   float fresnelLight = fresnel(normal, dir);
@@ -184,7 +215,9 @@ void main(){
     float fov = 4.6;
     vec3 dir = camera * normalize(vec3(uv, fov));
 
-    float shortestDistanceToScene = raymarching(eye, dir);
+    vec2 distanceAndMat = raymarching(eye, dir);
+    float shortestDistanceToScene = distanceAndMat.x;
+    float mat = distanceAndMat.y;
 
     vec3 color;
     vec3 bgColor = vec3(0.1, 0.35, 0.75);
@@ -192,7 +225,7 @@ void main(){
     if (shortestDistanceToScene < FAR_CLIP - EPSILON) {
         vec3 collision = (eye += (shortestDistanceToScene*0.995) * dir );
         float shadow  = softshadow(collision, lightDirection, 0.02, 2.5 );
-        color = calculateColor(collision, dir);
+        color = calculateColor(collision, dir, mat);
         
         shadow = mix(shadow, 1.0, 0.7);
         color = color * shadow;
@@ -204,4 +237,5 @@ void main(){
 
     //gl_FragColor = vec4(clamp(color,0.0,1.0) , 1.0);
     gl_FragColor = vec4(color , 1.0);
+
 }
